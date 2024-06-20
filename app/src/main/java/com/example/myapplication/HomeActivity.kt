5e83ac8a06c22f80aaeda735e609d909b8f5c59c
tharunmadhav <tharunmadhav.b2021@vitstudent.ac.in>
2024-06-20 10:43:21 +0530

    recycler vie all parts 1-4

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,3 +1,4 @@
+package com.example.myapplication
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -9,59 +10,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 
-class HomeActivity : AppCompatActivity() {
-    class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
-        AdapterView.OnItemClickListener {
-        var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
-        lateinit var mySpinner: Spinner
-        lateinit var myListview: ListView
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
-            setContentView(R.layout.activity_list_item)
-            mySpinner = findViewById(R.id.spinner) //taking handle
-            myListview = findViewById(R.id.listView)
-            myListview.isClickable = true
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    var data = arrayOf("india","hindi","australia","peacock","blue")
+    var dataArray = arrayOf("india","hindi","australia","peacock","blue")
+    var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
+    lateinit var mySpinner: Spinner
+    lateinit var myRecycler: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        mySpinner = findViewById(R.id.spinner) //taking handle
+        myRecycler = findViewById(R.id.recyclerView)
+        myRecycler.layoutManager = LinearLayoutManager(this)
+        var wordsAdapter = WordsAdapter(dataArray)
+        myRecycler.adapter = wordsAdapter
 
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
-
-            mySpinner.onItemSelectedListener = this
-            myListview.setOnItemClickListener(this)
-            //get intent which sttarted this activity
-            //get the extras from that intent
-            //get the string with mykey
-            var data = intent.extras?.getString("mykey")
-            /*  var data = intent.extras?.getString("mykey")
-              Log.i("homeactivity",data.toString())
-              //put the data either in a log or on the textview
-              var homeTextView:TextView = findViewById(R.id.tvHome)
-              homeTextView.setText(data)
-              homeTextView.setText(data)*/
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-
-        override fun onItemSelected(
-            adpater: AdapterView<*>?,
-            view: View?,
-            position: Int,
-            id: Long
-        ) {
-            var item: String = adpater?.getItemAtPosition(position).toString()
-            Log.i(TAG, item)
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onItemClick(adpater: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            var item: String = adpater?.getItemAtPosition(position).toString()
-            Log.i(TAG, item)
-        }
+        mySpinner.onItemSelectedListener = this
+        //get intent which sttarted this activity
+        //get the extras from that intent
+        //get the string with mykey
+        /*  var data = intent.extras?.getString("mykey")
+          Log.i("homeactivity",data.toString())
+          //put the data either in a log or on the textview
+          var homeTextView:TextView = findViewById(R.id.tvHome)
+          homeTextView.setText(data)*/
+    }
+    override fun onItemSelected(adpater: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var item:String = adpater?.getItemAtPosition(position).toString()
+        Log.i(TAG, item )
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
