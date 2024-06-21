8565972824bc450787c70f5634cc6ec589f5af8f
tharunmadhav <tharunmadhav.b2021@vitstudent.ac.in>
2024-06-21 11:08:59 +0530

    databinding

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -3,51 +3,51 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.ActivityHomeBinding
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import com.example.myapplication.MarsAdapter
-import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityHomeBinding
 import com.example.myapplication.network.MarsApi
 import com.example.myapplication.network.MarsPhoto
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import okhttp3.Dispatcher
+//wet code --- dry code -- repetative lines removed
 class HomeActivity : AppCompatActivity(){
     var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
-    lateinit var marsRecyclerView:RecyclerView
+
     private lateinit var binding: ActivityHomeBinding
+    val photoMarsDatabinding = MarsPhoto("007","moonimage.com")
 
-    //late init var marsRecyclerView:RecyclerView
-
+    //lateinit var marsRecyclerView:RecyclerView
     lateinit var marsAdapter: MarsAdapter
     lateinit var photos:List<MarsPhoto>
-    lateinit var imageView: ImageView
+    //  lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        imageView = findViewById(R.id.imageView)
-        marsRecyclerView = findViewById(R.id.recyclerViewUrls)
-        marsRecyclerView.layoutManager = LinearLayoutManager(this)
         // setContentView(R.layout.activity_home)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val binding: ActivityHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        /* var homeTextView:TextView = findViewById(R.id.tvHome)
+         homeTextView.setText(photoMarsDatabinding.imgSrc)*/
+        binding.marsphotoxml = photoMarsDatabinding
 
+        // imageView = findViewById(R.id.imageView)
         // imageView = findViewById(R.id.imageView)
         // marsRecyclerView = findViewById(R.id.recyclerViewUrls)
         binding.recyclerViewUrls.layoutManager = LinearLayoutManager(this)
-
         photos = ArrayList()
         marsAdapter = MarsAdapter(photos)
-        marsRecyclerView.adapter = marsAdapter
         binding.recyclerViewUrls.adapter = marsAdapter
         // marsAdapter = MarsAdapter(photos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -59,14 +59,14 @@ class HomeActivity : AppCompatActivity(){
     private fun getMarsPhotos() {
         GlobalScope.launch(Dispatchers.Main) {
             //launching coroutines on the main thread is not advisable
-            val listMarsPhotos =   MarsApi.retrofitService.getPhotos()
+            var listMarsPhotos =   MarsApi.retrofitService.getPhotos()
             // photos = listMarsPhotos
             marsAdapter.listMarsPhotos = listMarsPhotos
             //import coil.load
-            imageView.load(listMarsPhotos.get(0).imgSrc)
             marsAdapter.notifyDataSetChanged()
             //   var tvHome:TextView = findViewById(R.id.tvHome)
 //            tvHome.setText(listMarsPhotos.get(1).imgSrc)
+            //  binding.imageView.load()
             Log.i("homeactiviy",listMarsPhotos.size.toString())
             Log.i("homeactivity-url",listMarsPhotos.get(1).imgSrc)
         }
