d1ca6d186e67e1013eb6e787d96f5279d457ea92
tharunmadhav <tharunmadhav.b2021@vitstudent.ac.in>
2024-06-21 10:31:08 +0530

    viewbinding

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityHomeBinding
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,6 +22,10 @@ import okhttp3.Dispatcher
 class HomeActivity : AppCompatActivity(){
     var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
     lateinit var marsRecyclerView:RecyclerView
+    private lateinit var binding: ActivityHomeBinding
+
+    //late init var marsRecyclerView:RecyclerView
+
     lateinit var marsAdapter: MarsAdapter
     lateinit var photos:List<MarsPhoto>
     lateinit var imageView: ImageView
@@ -31,9 +36,19 @@ class HomeActivity : AppCompatActivity(){
         imageView = findViewById(R.id.imageView)
         marsRecyclerView = findViewById(R.id.recyclerViewUrls)
         marsRecyclerView.layoutManager = LinearLayoutManager(this)
+        // setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // imageView = findViewById(R.id.imageView)
+        // marsRecyclerView = findViewById(R.id.recyclerViewUrls)
+        binding.recyclerViewUrls.layoutManager = LinearLayoutManager(this)
+
         photos = ArrayList()
         marsAdapter = MarsAdapter(photos)
         marsRecyclerView.adapter = marsAdapter
+        binding.recyclerViewUrls.adapter = marsAdapter
         // marsAdapter = MarsAdapter(photos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -44,7 +59,7 @@ class HomeActivity : AppCompatActivity(){
     private fun getMarsPhotos() {
         GlobalScope.launch(Dispatchers.Main) {
             //launching coroutines on the main thread is not advisable
-            var listMarsPhotos =   MarsApi.retrofitService.getPhotos()
+            val listMarsPhotos =   MarsApi.retrofitService.getPhotos()
             // photos = listMarsPhotos
             marsAdapter.listMarsPhotos = listMarsPhotos
             //import coil.load
