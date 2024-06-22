58c65ef9c1867fae344d023626e32fd70f8d92d8
tharunmadhav <tharunmadhav.b2021@vitstudent.ac.in>
2024-06-22 10:40:06 +0530

    roomdb---2

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,61 +15,45 @@ import com.example.myapplication.database.Item
 import com.example.myapplication.database.ItemDao
 import com.example.myapplication.database.ItemRoomDatabase
 import com.example.myapplication.databinding.ActivityHomeBinding
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.launch
 
-class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
-    private val TAG = HomeActivity::class.java.simpleName
-    private lateinit var mySpinner: Spinner
-    private lateinit var myListView: ListView
+class HomeActivity : AppCompatActivity() {
+    var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var dao: ItemDao
-
+    lateinit var dao: ItemDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        mySpinner = findViewById(R.id.spinner)
-        myListView = findViewById(R.id.listView)
-        myListView.isClickable = true
-
-        val database = ItemRoomDatabase.getDatabase(this)
+        var  database = ItemRoomDatabase.getDatabase(this)
         dao = database.itemDao()
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        binding.btnDbInsert.setOnClickListener {
+        binding.btnDbInsert.setOnClickListener{
             insertDataDb()
         }
 
-        mySpinner.onItemSelectedListener = this
-        myListView.setOnItemClickListener(this)
+        binding.btnFind.setOnClickListener{
+            findItemDb(21)
+        }
     }
 
-    override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val item: String = adapter?.getItemAtPosition(position).toString()
-        Log.i(TAG, item)
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        // Handle case where nothing is selected, if necessary
-    }
-
-    override fun onItemClick(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val item: String = adapter?.getItemAtPosition(position).toString()
-        Log.i(TAG, item)
+    private fun findItemDb(id: Int) {
+        GlobalScope.launch(Dispatchers.Main) {
+            var item = dao.getItem(id).first()
+            binding.tvHome.setText(item.itemName)
+        }
     }
 
     private fun insertDataDb() {
         GlobalScope.launch {
-            val item = Item(21, "fruits", 11.11, 11)
+            var item = Item(21,"fruits",11.11,11)
             dao.insert(item)
         }
     }
